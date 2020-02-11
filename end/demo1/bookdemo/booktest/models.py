b1d557106d78885983d8b855cea1e38b431708a5
lyb0308 <111@qq.com>
2020-02-11 14:42:40 +0800

    我的项目 我的应用

---
+++ end/demo1/bookdemo/booktest/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# MVT M数据模型
+# ORM M数据模型
+# 在此处编写应用的模型类
---
+++ end/demo1/bookdemo/booktest/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+# MVT v
+# 在此处接受请求 处理数据 返回响应
